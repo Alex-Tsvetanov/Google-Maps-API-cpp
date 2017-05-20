2820cbed4189f83cb221135bd7d8dd041c7c4331
Alex-Tsvetanov <alex_tsvetanov_2002@abv.bg>
2017-05-20 20:13:30 +0300

    fix documentation (1)

--- doc/html/search/pages_3.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['readme',['Readme',['../md__home_alex_GIT_My_have_repo_LovecMaps_PublicTransport_api_lib_Google_Maps_API_external_libs_curlpp_Readme.html',1,'']]],
-  ['readme',['README',['../md__home_alex_GIT_My_have_repo_LovecMaps_PublicTransport_api_lib_Google_Maps_API_external_libs_json_README.html',1,'']]],
-  ['readme',['README',['../md__home_alex_GIT_My_have_repo_LovecMaps_PublicTransport_api_lib_Google_Maps_API_external_libs_j43aed30503de71d5008a8e6c59f4a13d.html',1,'']]],
-  ['readme',['README',['../md__home_alex_GIT_My_have_repo_LovecMaps_PublicTransport_api_lib_Google_Maps_API_README.html',1,'']]]
-];
