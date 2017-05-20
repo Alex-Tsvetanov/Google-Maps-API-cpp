56aa0ac437e7594c628682ffedbf6cc17b0963ec
Alex-Tsvetanov <14363901+Alex-Tsvetanov@users.noreply.github.com>
2017-05-20 20:11:38 +0300

    add direction api client

--- doc/html/search/all_7.js
+++ doc/html/search/all_7.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['lang',['lang',['../d6/d2d/a00047.html#a4854d3b172e3e823b647897fe8b24f44',1,'Google::Maps::API::client']]],
   ['langs_2ehpp',['langs.hpp',['../db/db2/a00020.html',1,'']]],
-  ['latitude',['latitude',['../d3/d8b/a00059.html#af6b6537d2ea2525753d9e2f170fb2622',1,'Google::Maps::point::latitude()'],['../d3/d8b/a00059.html#a4d2dcdbfa413c92d7a11fb7a280b5238',1,'Google::Maps::point::Latitude() const']]],
+  ['latitude',['Latitude',['../d3/d8b/a00059.html#a4d2dcdbfa413c92d7a11fb7a280b5238',1,'Google::Maps::point::Latitude() const'],['../d3/d8b/a00059.html#af6b6537d2ea2525753d9e2f170fb2622',1,'Google::Maps::point::latitude()']]],
   ['log_2ehpp',['log.hpp',['../d2/de7/a00014.html',1,'']]],
-  ['longitude',['longitude',['../d3/d8b/a00059.html#a488d9eb28fe2486d91173646ab620202',1,'Google::Maps::point::longitude()'],['../d3/d8b/a00059.html#a0fd1c4ec3f383385b8981629e0acb188',1,'Google::Maps::point::Longitude() const']]]
+  ['longitude',['Longitude',['../d3/d8b/a00059.html#a0fd1c4ec3f383385b8981629e0acb188',1,'Google::Maps::point::Longitude() const'],['../d3/d8b/a00059.html#a488d9eb28fe2486d91173646ab620202',1,'Google::Maps::point::longitude()']]]
 ];
